b47b8d0028fdc6597da0fb0f130cf1f69c2f9e96
Sarfraz <sarfrazsiddiqi19@gmail.com>
2022-10-15 15:26:57 +0530

    Adding changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import ModalProvider from './context/ModalContext';
+import PlaygroundProvider from './context/PlaygroundContext';
 import HomeScreen from './screens/HomeScreen';
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <div>
-      <GlobalStyles/>
-      <HomeScreen />
-    </div>
+    <PlaygroundProvider>
+      <ModalProvider>
+         <GlobalStyles/>
+         <HomeScreen />
+      </ModalProvider>
+      </PlaygroundProvider>
   );
 }
 export default App;
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useContext, useState } from "react";
 import LeftPane from './LeftPane'
 import RightPane from './RightPane'
 import styled from 'styled-components'
+import Modal from '../../component/Modal'
+import { ModalContext } from '../../context/ModalContext'
 
 const HomeScreenContainer = styled.div`
-display: grid;
-grid-template-columns: 1fr 1.5fr;
+position: relative;
+width: 100%
 height:100vh;`
 
 const HomeScreen = () => {
+  const ModalFeatures = useContext(ModalContext)!;
+  const isOpen = ModalFeatures.isOpen;
+
   return (
+  
     <HomeScreenContainer>
         <LeftPane />
         <RightPane />
+        {isOpen.value === true ? <Modal /> : <></>}
         </HomeScreenContainer>
   )
 }
